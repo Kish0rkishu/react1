657544a918f0980d24c51406cff0cc0993489328
kishorkishu <kishor.n382003@gmail.com>
2023-11-20 10:16:17 +0530

    react1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Body from './components/Body'
-import Body1 from './components/Body1'
-const App = () => {
+import Navigation from './components/Navigation.js'
+import Home from './components/Home.js'
+import Contact from './components/Contact.js'
+import TodoList from './components/ToDoList.js'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+
+function App() {
   return (
     <div>
-      <Header/>
-      <Footer/>
-      <Body/>
-      <Body1/>
-      </div>
-
+      <Router>
+        <div>
+           <Navigation></Navigation>
+        </div>
+        <Routes>
+          <Route path='/home' element={<Home/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='/todolist' element={<TodoList/>}/>
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
 export default App
-
-
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>SIET
+        <p>About SIET Tumkur
+Shridevi Institute of Engineering and Technology (SIET) is a private engineering college located in Tumkur, Karnataka. It was established in the year 2002 and is affiliated to Visvesvaraya Technological University, Belagavi. The institute is approved by the All India Council for Technical Education (AICTE). SIET offers undergraduate, postgraduate and Ph.D. courses in engineering and technology. The courses offered are Bachelor of Engineering (BE) in various disciplines, Master of Technology (MTech) in selected disciplines, Master of Business Administration (MBA) and Ph.D. Shridevi Institute of Engineering and Technology (SIET) offers admission to various undergraduate and postgraduate courses through competitive exams such as COMEDK UGET/ KCET/ GATE/ Karnataka PGCET.</p>
+</div>
+  )
+}
+
+export default Home
